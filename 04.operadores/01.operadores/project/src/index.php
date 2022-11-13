68e09930c70aadc2c89e8dc5ce07f075b05b7c82
rpxic <rpc2works@gmail.com>
2022-11-13 23:37:05 +0100

    operadores

---
+++ 04.operadores/01.operadores/project/src/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>operadores</h1>
+  <?php
+  // código php
+  ?>
+  <?php
+    $a = 2 + 3 * 8;
+    var_dump($a);
+    $b = (2 + 3) * 8; 
+    var_dump($b);
+    $c = 2 + (3 * 8); 
+    var_dump($c);
+    // otro ejemplo
+    $a = 2;
+    $b = 3;
+    $c = 8;
+    var_dump($a - $b - $c);
+    var_dump(($a - $b) - $c);
+    var_dump($a - ($b - $c));
+    // operador de asignación, '='
+    $c = 8;
+    echo $a = $b = $c;
+  ?>
+</body>
+</html>
