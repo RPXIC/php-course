e6ccd3745e2e0e3e6232c3b05f30aaceba0d3283
rpxic <rpc2works@gmail.com>
2022-11-17 22:46:23 +0100

    funciones php

---
+++ 07.funciones_php/05.include/project/src/index-alternative.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <?php
+  include './menu.php';
+  ?>
+  <h1>include alternativo</h1>
+
+  <p>Some text.</p>
+
+  <?php
+  include './footer.php';
+  ?>
+    <a href="./">al index</a>
+</body>
+</html>
---
+++ 07.funciones_php/05.include/project/src/menu.php
@@ -0,0 +1,3 @@
+<a href="./">index</a>
+
+<a href="./index-alternative.php">index alternativo</a>
